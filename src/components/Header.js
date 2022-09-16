99d644f509eb4661b387564ad738982789c8f1de
Igor Kolpakov <gragass00@yandex.ru>
2022-09-16 16:54:33 +0300

    added check in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import logo from '../images/logos/logo.svg'
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+
 
 function Header() {
+    const location = useLocation()
+
     return (
         <header className="header">
             <img src={logo} alt="Логотип Место Russia" className="header__logo"/>
-            <Link to='sing-up'>Регистрация</Link>
+
+            {location.pathname === "/sign-up" ? <Link to='/sign-in'>Вход</Link> : <Link to='/sign-up'>Регистрация</Link>}
         </header>
     )
 }
